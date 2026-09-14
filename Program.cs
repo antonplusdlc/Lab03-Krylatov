06f4ce6f15f5b2287875fea45973622d15ac80c1
antonplusdlc <halloweenbsdevs@gmail.com>
2026-09-14 22:56:21 +0300

    Lab03: compound assignment operators

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счет");
+
+double balance = 1000;
+Console.WriteLine($"Starting balance: {balance}");
+
+balance += 500; // ++
+Console.WriteLine($"Popolnenie na 500: {balance}");
+
+balance -= 200; // buy
+Console.WriteLine($"after buying: {balance}");
+
+balance *= 1.05; // + 5%
+Console.WriteLine($"+ 5%: {balance}");
+
+balance /= 2; // партнер украл половину
+Console.WriteLine($"после кражи: {balance}");
